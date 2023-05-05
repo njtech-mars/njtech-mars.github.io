0471fa9cfaae4c72ca536f27e11d8f3a0d4a3d52
MR-Addict <2750417853@qq.com>
2023-05-05 13:12:39 +0800

    update blog tag style

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,15 +1,37 @@
 <script lang="ts">
+  import FaClock from 'svelte-icons/fa/FaRegClock.svelte';
+  import FaRegUser from 'svelte-icons/fa/FaRegUser.svelte';
+
   import type { PageData } from './$types';
-  import Metadata from '$components/Metadata/Metadata.svelte';
+  import { formatDate, timeAgo } from '$lib/utils';
 
   export let data: PageData;
 </script>
 
-<main class="w-full flex-1 frame py-5 md:py-10 flex flex-col gap-5 animate-slideFromBottom">
-  <header class="flex flex-col gap-1">
-    <h1 class="text-2xl font-semibold">{data.title}</h1>
+<main class="w-full flex-1 frame py-5 md:py-10 flex flex-col items-center gap-5 animate-slideFromBottom">
+  <header class="flex flex-col items-center gap-3">
+    <h1 class="text-2xl md:text-3xl font-semibold">{data.title}</h1>
 
-    <Metadata post={data} />
+    <div class="flex flex-row items-center gap-1 text-gray-500 text-sm">
+      <div class="flex flex-row items-center gap-0.5">
+        <div class="w-3 h-3"><FaClock /></div>
+        <div>{formatDate(data.date)}</div>
+        <div>(更新于{timeAgo(data.update)})</div>
+      </div>
+
+      <p>•</p>
+
+      <div class="flex flex-row items-center gap-0.5">
+        <div class="w-3 h-3"><FaRegUser /></div>
+        <div>{data.author}撰写</div>
+      </div>
+    </div>
+
+    <div class="flex flex-row flex-wrap gap-2 text-sm">
+      {#each data.tags as tag (tag)}
+        <span class="py-1 px-2 rounded-2xl text-orange-600 bg-orange-600/20">&num;{tag}</span>
+      {/each}
+    </div>
   </header>
 
   <article class="markdown">
