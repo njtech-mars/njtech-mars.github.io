6377254651168481e5dbab1c28acbddb58769f21
MR-Addict <2750417853@qq.com>
2023-06-18 12:51:44 +0800

    docs: update send project post

--- src/routes/components/Articles/Articles.svelte
+++ src/routes/components/Articles/Articles.svelte
@@ -5,7 +5,7 @@
   const posts = getPosts().slice(0, 5);
 </script>
 
-<div class="flex flex-col gap-2">
+<div class="flex flex-col gap-2 opacity-0 animate-slideFromBottom animation-delay-800">
   <div class="w-full flex flex-row justify-between items-end">
     <h1 class="text-2xl font-semibold">最新文章</h1>
     <a href="/blog" title="文章动态" class="text-blue-600 hover:underline">更多...</a>
